cc637257f748ffb8c4778359e401d14112b09d8e
JackM1994 <36908600+JackM1994@users.noreply.github.com>
2020-06-18 10:52:07 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+//require csvtojson module
+const CSVToJSON = require('csvtojson');
+
+//convert users.csv file to JSON array
+CSVToJSON().fromFile('customer-data.xls')
+    .then(customers=>{
+        //log the customers array
+        console.log(customers);
+    }).catch(err => {
+        //log error 
+        console.log(err);
+    });
